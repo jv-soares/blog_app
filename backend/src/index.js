1f1a61c3ae376d7f96ad6bde40b420fe77a798f3
João Vitor <j.soares@aparkado.de>
2025-04-13 16:27:12 +0200

    move backend code into src

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,5 +3,5 @@ const config = require('./utils/config.js');
 const logger = require('./utils/logger.js');
 
 app.listen(config.port, () => {
-    logger.info(`Server running on port ${config.port}`);
+  logger.info(`Server running on port ${config.port}`);
 });
