ebfd4604ca5f682d1b112f75b7d02b66c6ebbcb5
João Vitor <j.soares@aparkado.de>
2025-04-26 12:44:10 +0200

    revert backend change

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,5 @@ const config = require('./src/utils/config');
 const logger = require('./src/utils/logger');
 
 app.listen(config.port, () => {
-  console.log('testing workflow');
   logger.info(`Server running on port ${config.port}`);
 });
